192a41912410a41895f7e35bb4c5eba1f557478d
hibasheikh90 <hayasheikh109@gmail.com>
2024-07-13 10:48:37 +0500

    first commit

---
+++ index.ts
@@ -0,0 +1,211 @@
+import inquirer from "inquirer";
+// Define the student class
+
+class Student{
+    static counter = 10000;
+    id: number;
+    name: string;
+    courses: string[];
+    balance: number;
+
+    constructor(name:string){
+        this.id = Student.counter++;
+        this.name = name;
+        this.courses = [];
+        this.balance = 100;
+    }
+
+    //method to enroll a student in a course 
+    enroll_course(course: string){
+        this.courses.push(course);
+    }
+
+    //method to view a student balance
+    view_balance(){
+        console.log(`Balance for ${this.name} : $${this.balance}`);
+    }
+
+    //method to pay student fees
+    pay_fees(amount: number){
+        this.balance -= amount;
+        console.log(`$${amount} Fees paid successfully for ${this.name}`);
+    }
+    //method to display student status
+    show_status(){
+        console.log(`ID: ${this.id}`);
+        console.log(`Name: ${this.name}`);
+        console.log(`Courses: ${this.courses}`);
+        console.log(`Balance: ${this.balance}`);
+        
+    }
+}
+
+// Defining a student manager class to manage student
+
+class student_manager{
+    students: Student[]
+
+
+    constructor(){
+        this.students = [];
+    }
+    //method to add a new student
+
+    add_student(name: string){
+        let student = new Student(name);
+        this.students.push(student);
+        console.timeLog(`Student ${name} added successfully. ${student.id}`)
+    }
+
+    //methode to enroll in a course
+
+    enroll_student(student_id:number, course: string){
+        let student = this.find_student(student_id);
+        if(student){
+            student.enroll_course(course);
+            console.log(`${student.name} enrolled in ${course} successfully`);
+        }
+    }
+    // method to view student balance
+
+    view_student_balance(student_id: number){
+        let student = this.find_student(student_id);
+        if(student){
+            student.view_balance();
+        }
+        else{
+            console.log("Student not found. Please enter a correct student id")
+        }
+
+    }
+
+    //method to pay student fees
+
+    pay_student_fees(student_id: number, amount: number){
+        let student = this.find_student(student_id);
+        if(student){
+            student.pay_fees(amount);
+        }
+        else{
+            console.log("Student not found. Please enter a correct student id") 
+        }
+
+    }
+
+    //method to display student status
+
+    show_student_status(student_id: number){
+        let student = this.find_student(student_id);
+        if(student){
+            student.show_status();
+        }
+    }
+    
+
+    //method to find a student by student_id
+    find_student(student_id: number){
+        return this.students.find(std => std.id === student_id )
+
+    }
+}
+
+// main functionto run program
+
+async function main() {
+    console.log("Welcome To 'Hiba Sheikh' - Student Management System");
+    console.log("-".repeat(50));
+
+    let student_Manager = new student_manager();
+
+    // while loop to keep program running
+    while(true){
+        let choice = await inquirer.prompt([
+            {
+                name: "choice",
+                type: "list",
+                message: "Select an optin",
+                choices: [
+                    "Add Student",
+                    "Enroll Student",
+                    "View student Balance",
+                    "Pay Fees",
+                    "Show Status",
+                    "Exit"
+                ]
+            }
+        ]);
+
+        //using switch case to handle  user choice
+        switch(choice.choice){
+            case "Add Student":
+            let name_input = await inquirer.prompt([
+                {
+                    name: "name",
+                    type: "input",
+                    message: "Enter a student name",
+                }
+            ]);
+            student_Manager.add_student(name_input.name);
+            break;
+            case "Enroll Student":
+                let course_input = await inquirer.prompt([
+                    {
+                        name: "Student_id",
+                        type: "number",
+                        message: "Enter student ID",
+                    },
+                    {
+                        name: "Course",
+                        type: "input",
+                        message: "Enter a course name",
+                    }
+                ]);
+                student_Manager.enroll_student(course_input.student_id, course_input.course);
+                break;
+            case "View student Balance":
+                let balance_input = await inquirer.prompt([
+                    {
+                        name: "student_id",
+                        type: "number",
+                        message: "Enter a student ID",
+                    }
+                ]);
+                student_Manager.view_student_balance(balance_input.student_id);
+                break;
+            case "Pay Fees":
+                let fees_input = await inquirer.prompt([
+                    {
+                        name: "student_id",
+                        type: "number",
+                        message: "Enter a student ID",
+                    },
+                    {
+                        name: "amount",
+                        type: "number",
+                        message: "Enter the amount to pay",
+                    }
+                ]);
+                student_Manager.pay_student_fees(fees_input.student_id, fees_input.amount);
+                break;
+            case "Show Status":
+                let status_input = await inquirer.prompt([
+                    {
+                        name: "student_id",
+                        type: "number",
+                        message: "Enter a student ID",
+                    }
+                ]);
+                student_Manager.show_student_status(status_input.student_id);
+                break;
+            case "Exit":
+                console.log("Exiting...");
+                process.exit();   
+        }
+    }  
+}
+
+// calling a main function
+
+main();
+
+
